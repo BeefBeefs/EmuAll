6cdd0d0bf020449e3e2f7b01f4075e11d5c5c468
BeefBeefs <104284075+BeefBeefs@users.noreply.github.com>
2026-09-11 12:54:22 -0400

    Bump version for PS2 and Dolphin fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.beefbeefs.emuall"
         minSdk = 26
         targetSdk = 35
-        versionCode = 19
-        versionName = "0.8.9"
+        versionCode = 20
+        versionName = "0.9.0"
 
         externalNativeBuild {
             cmake {
